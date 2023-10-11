076e3a78f8cd4b4b1a59f63337053721ff3231c8
dontjumpshot <143997159+dontjumpshot@users.noreply.github.com>
2023-10-11 21:47:19 +0800

    Update RenderTetrahedron.h

--- Src/DelaunayTriangulation3D/DelaunayTriangulation3D/RenderTetrahedron.h
+++ Src/DelaunayTriangulation3D/DelaunayTriangulation3D/RenderTetrahedron.h
@@ -11,13 +11,16 @@ namespace delaunay3d
 		cRenderTetrahedron();
 		virtual ~cRenderTetrahedron();
 
+		//初始化函数，用于传入渲染器和四面体，并创建顶点缓冲区存储四面体的顶点数据
 		bool Init(graphic::cRenderer &renderer, const cTetrahedron &tet);
+
+		//绘制函数，用于将四面体绘制到屏幕上。可以选择是否绘制法向量和边缘。
 		void Render(graphic::cRenderer &renderer, const bool renderNorm=false, const bool renderEdge=false);
 
-		cTetrahedron m_tet;
-		graphic::cVertexBuffer m_vtxBuffer;
-		graphic::cVertexBuffer m_normalBuffer;
-		graphic::cVertexBuffer m_edgeBuffer;
+		cTetrahedron m_tet;  //存储四面体的信息
+		graphic::cVertexBuffer m_vtxBuffer;  //存储四面体顶点数据的顶点缓冲区
+		graphic::cVertexBuffer m_normalBuffer;  //存储四面体法向量数据的顶点缓冲区
+		graphic::cVertexBuffer m_edgeBuffer;  //存储四面体边缘数据的顶点缓冲区
 	};
 
 }
