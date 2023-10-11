2f986db40b1ae2950a0be614ced8f01a3cb14dc3
dontjumpshot <143997159+dontjumpshot@users.noreply.github.com>
2023-10-11 21:43:53 +0800

    Update CircumCenter.h

--- Src/DelaunayTriangulation3D/DelaunayTriangulation3D/CircumCenter.h
+++ Src/DelaunayTriangulation3D/DelaunayTriangulation3D/CircumCenter.h
@@ -3,7 +3,7 @@
 #pragma once
 
 //using namespace QM;
-void TetCircumCenter( float *a, float *b, float *c, float *d, float *r, float *p);
-void TriCircumCenter2D(float *a, float *b, float *c, float *r, float *p);
-void TriCircumCenter3D(float *a, float *b, float *c, float *r, float *p);
-void TriCircumCenter3D(float *a, float *b, float *c, float *r);
+void TetCircumCenter( float *a, float *b, float *c, float *d, float *r, float *p);  //计算四面体的外接球圆心。
+void TriCircumCenter2D(float *a, float *b, float *c, float *r, float *p);  //计算二维三角形的外接圆圆心。
+void TriCircumCenter3D(float *a, float *b, float *c, float *r, float *p);  //计算三维三角形的外接圆圆心。
+void TriCircumCenter3D(float *a, float *b, float *c, float *r);  //计算三维三角形的外接圆圆心。
